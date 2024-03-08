3d5f2c3ddd337ee7827e6138e6b644b93919fcc5
abhinna1 <manandharabhinna@gmail.com>
2024-03-08 23:52:02 +0545

    code formatted

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 
-
 INT, OPERATOR, ADD, SUB = "int", "operator", '+', '-'
+
 
 class Token:
     def __init__(self, key, value):
         self.type = key
         self.value = value
-    
+
     def __str__(self):
         return str(self.value)
-    
+
     def __repr__(self):
         return str(self.value)
+
 
 class Operator(Token):
     def __init__(self):
@@ -19,19 +20,20 @@ class Operator(Token):
 
     def operate(self, left, right):
         raise Exception("Not Implemented.")
-        
+
 
 class AddOperator(Operator):
     def __init__(self):
         self.value = ADD
-        
+
     def operate(self, left, right):
         return left + right
-    
+
+
 class SubstractOperator(Operator):
     def __init__(self):
         self.value = SUB
-        
+
     def operate(self, left, right):
         return left - right
 
@@ -47,23 +49,25 @@ def operator_factory(op: Operator):
     elif op == SUB:
         return SubstractOperator()
 
+
 def int_token_factory(value: int):
     return Token(
         INT,
         int(value)
     )
 
+
 class Interpreter:
     def __init__(self, code: str):
         self.code = code
         self.curr = 0
-        
+
         self.adder = AddOperator()
         self.subber = SubstractOperator()
-    
+
         self.stack = []
         self.operator = None
-    
+
     def process(self):
         for i in self.code:
             if self.operator:
@@ -75,7 +79,7 @@ class Interpreter:
                 self.stack.append(current_token)
             else:
                 self.operator = operator_factory(i)
-    
+
     def get_stack(self):
         print(self.stack)
 
